e46a9b5bf1959bb787faf8cb257d5e4c7f1b0982
Steven Kapturowski <skapturowski@apple.com>
2017-03-16 09:24:46 -0700

    add atari architecture to match TRPO paper

--- networks/network.py
+++ networks/network.py
@@ -31,8 +31,13 @@ class Network(object):
                 self.input_ph = tf.placeholder('float32', [self.batch_size]+self.input_shape+[4], name='input')
                 self.w1, self.b1, self.o1 = layers.fc('fc1', layers.flatten(self.input_ph), 40, activation='relu')
                 self.w2, self.b2, self.o2 = layers.fc('fc2', self.o1, 40, activation='relu')
-                
                 self.ox = self.o2
+            elif self.arch == 'ATARI-TRPO':
+                self.input_ph = tf.placeholder('float32',[self.batch_size, 84, 84, 4], name='input')
+                self.w1, self.b1, self.o1 = layers.conv2d('conv1', self.input_ph, 16, 4, 4, 2)
+                self.w2, self.b2, self.o2 = layers.conv2d('conv2', self.o1, 16, 4, 16, 2)
+                self.w3, self.b3, self.o3 = layers.fc('fc3', layers.flatten(self.o2), 20, activation='relu')
+                self.ox = self.o3
             elif self.arch == 'NIPS':
                 self.input_ph = tf.placeholder('float32',[self.batch_size, 84, 84, 4], name='input')
                 self.w1, self.b1, self.o1 = layers.conv2d('conv1', self.input_ph, 16, 8, 4, 4)
