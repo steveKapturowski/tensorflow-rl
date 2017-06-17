22741738e4bbf721885b86562fd7acd91bb1ba2a
Steven Kapturowski <myrealnamewascensored@gmail.com>
2017-06-17 15:10:29 -0700

    update pgq learner

--- algorithms/pgq_actor_learner.py
+++ algorithms/pgq_actor_learner.py
@@ -20,6 +20,7 @@ class BasePGQLearner(BaseA3CLearner):
 
         super(BasePGQLearner, self).__init__(args)
 
+        self.q_update_counter = 0
         self.replay_size = args.replay_size
         self.pgq_fraction = args.pgq_fraction
         self.batch_update_size = args.batch_update_size
@@ -87,7 +88,7 @@ class BasePGQLearner(BaseA3CLearner):
                 self.terminal_indicator: is_terminal.astype(np.int),
             }
         )
-        return batch_grads
+        self.apply_gradients_to_shared_memory_vars(batch_grads)
 
 
 class PGQLearner(BasePGQLearner):
@@ -107,122 +108,21 @@ class PGQLearner(BasePGQLearner):
         return new_action, network_output_v, network_output_pi
 
 
-    def train(self):
-        """ Main actor learner loop for advantage actor critic learning. """
-        logger.debug("Actor {} resuming at Step {}".format(self.actor_id, 
-            self.global_step.value()))
+    def apply_update(self, states, actions, targets, advantages):
+        feed_dict={
+            self.local_network.input_ph: states,
+            self.local_network.selected_action_ph: actions,
+            self.local_network.critic_target_ph: targets,
+            self.local_network.adv_actor_ph: advantages,
+        }
+        grads, entropy = self.session.run(
+            [self.local_network.get_gradients, self.local_network.entropy],
+            feed_dict=feed_dict)
+        self.apply_gradients_to_shared_memory_vars(grads)
 
-        s = self.emulator.get_initial_state()
-        steps_at_last_reward = self.local_step
-        total_episode_reward = 0.0
-        mean_entropy = 0.0
-        q_update_counter = 0
-        episode_start_step = 0
-        
-        while (self.global_step.value() < self.max_global_steps):
-            # Sync local learning net with shared mem
-            self.sync_net_with_shared_memory(self.local_network, self.learning_vars)
-            self.save_vars()
-            params = self.session.run(self.local_network.flat_vars)
-            self.assign_vars(self.batch_network, params)
+        self.q_update_counter += 1
+        if self.q_update_counter % self.q_update_interval == 0:
+            self.batch_q_update()
 
-            local_step_start = self.local_step
-            reset_game = False
-            episode_over = False
-            
-            states    = list()
-            rewards   = list()
-            actions   = list()
-            values    = list()
-            s_batch   = list()
-            a_batch   = list()
-            y_batch   = list()
-            adv_batch = list()
-            
-            while not (episode_over 
-                or (self.local_step - local_step_start 
-                    == self.max_local_steps)):
-                
-                # Choose next action and execute it
-                a, readout_v_t, readout_pi_t = self.choose_next_action(s)
-
-                if self.is_master() and (self.local_step % 100 == 0):
-                    logger.debug("pi={}, V={}".format(readout_pi_t, readout_v_t))
-                    
-                new_s, reward, episode_over = self.emulator.next(a)
-
-                if reward != 0.0:
-                    steps_at_last_reward = self.local_step
-
-
-                total_episode_reward += reward
-                # Rescale or clip immediate reward
-                reward = self.rescale_reward(reward)
-                self.replay_memory.append(s, a, reward, episode_over)
-                
-                rewards.append(reward)
-                states.append(s)
-                actions.append(a)
-                values.append(readout_v_t)
-                
-                s = new_s
-                self.local_step += 1
-                self.global_step.increment()
-            
-            # Calculate the value offered by critic in the new state.
-            if episode_over:
-                R = 0
-            else:
-                R = self.session.run(
-                    self.local_network.output_layer_v,
-                    feed_dict={self.local_network.input_ph:[new_s]})[0][0]
-                            
-             
-            sel_actions = []
-            for i in reversed(xrange(len(states))):
-                R = rewards[i] + self.gamma * R
-
-                y_batch.append(R)
-                a_batch.append(actions[i])
-                s_batch.append(states[i])
-                adv_batch.append(R - values[i])
-                
-                sel_actions.append(np.argmax(actions[i]))
-                
-
-            # Compute gradients on the local policy/V network and apply them to shared memory  
-            feed_dict={
-                self.local_network.input_ph: s_batch, 
-                self.local_network.critic_target_ph: y_batch,
-                self.local_network.selected_action_ph: a_batch,
-                self.local_network.adv_actor_ph: adv_batch,
-            }
-            grads, entropy = self.session.run(
-                [self.local_network.get_gradients, self.local_network.entropy],
-                feed_dict=feed_dict)
-
-            self.apply_gradients_to_shared_memory_vars(grads)
-
-
-            # q_grads = self.batch_q_update()
-            # if q_grads is not None:
-            #     grads = [p + q for p, q in zip(policy_grads, q_grads)]
-            # else:
-            #     grads = policy_grads
-            q_update_counter += 1
-            if q_update_counter % self.q_update_interval == 0:
-                q_grads = self.batch_q_update()
-                if q_grads is not None:
-                    # grads = [p + q for p, q in zip(grads, q_grads)]
-                    self.apply_gradients_to_shared_memory_vars(q_grads)
-
-
-            delta_old = local_step_start - episode_start_step
-            delta_new = self.local_step -  local_step_start
-            mean_entropy = (mean_entropy*delta_old + entropy*delta_new) / (delta_old + delta_new)
-            
-            s, mean_entropy, mean_value, episode_start_step, total_episode_reward, steps_at_last_reward = self.prepare_state(
-                s, mean_entropy, np.array(values).mean(), episode_start_step, total_episode_reward, steps_at_last_reward, sel_actions, episode_over)
-
-
+        return entropy
 
