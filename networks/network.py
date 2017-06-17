42adce695ba199bd5e7613ae6373ded5f3557716
Steven Kapturowski <myrealnamewascensored@gmail.com>
2017-06-17 14:45:34 -0700

    change select to where

--- networks/network.py
+++ networks/network.py
@@ -106,7 +106,7 @@ class Network(object):
     def _value_function_loss(self, diff):
         if self.clip_loss_delta > 0:
             # DEFINE HUBER LOSS
-            return 0.5 * tf.reduce_sum(tf.select(
+            return 0.5 * tf.reduce_sum(tf.where(
                 tf.abs(self.clip_loss_delta) < 1.0, tf.square(diff), tf.sqrt(2*self.clip_loss_delta)*tf.abs(diff)))
         else:
             return tf.nn.l2_loss(diff)
