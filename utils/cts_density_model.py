1b502a9c74c04f848fde34901921b81e7d866001
Steven Kapturowski <myrealnamewascensored@gmail.com>
2017-07-06 09:10:55 -0700

    fix context ordering in slow cts model

--- utils/cts_density_model.py
+++ utils/cts_density_model.py
@@ -5,10 +5,12 @@ from skimage.transform import resize
 
 
 class CTSDensityModel(object):
-	'''
+	"""
 	Implementation of CTS Density Model described in the paper
 	"Unifying Count-Based Exploration and Intrinsic Motivation" (https//arxiv.org/abs/1606.01868)
-	'''
+
+	Note that the cython version in fast_cts.pyx is significantly faster
+	"""
 	def __init__(self, height=21, width=21, beta=0.05):
 		self.beta = beta
 		self.factors = np.array([[CTS(4) for _ in range(width)] for _ in range(height)])
@@ -22,10 +24,10 @@ class CTSDensityModel(object):
 		log_recoding_prob = 0.0
 		for i in range(self.factors.shape[0]):
 			for j in range(self.factors.shape[1]):
-				context[3] = obs[i, j-1] if j > 0 else 0
-				context[2] = obs[i-1, j] if i > 0 else 0
-				context[1] = obs[i-1, j-1] if i > 0 and j > 0 else 0
-				context[0] = obs[i-1, j+1] if i > 0 and j < self.factors.shape[1]-1 else 0
+				context[0] = obs[i, j-1] if j > 0 else 0
+				context[1] = obs[i-1, j] if i > 0 else 0
+				context[2] = obs[i-1, j-1] if i > 0 and j > 0 else 0
+				context[3] = obs[i-1, j+1] if i > 0 and j < self.factors.shape[1]-1 else 0
 
 				log_prob += self.factors[i, j].update(context, obs[i, j])
 				log_recoding_prob += self.factors[i, j].log_prob(context, obs[i, j])
